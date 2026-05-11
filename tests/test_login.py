b907c536f4ae1b22062f87f8d54d63261142ace1
Mohamed Moadeb <moadeb.mohamed@gmail.com>
2026-05-11 14:49:20 +0200

    Adding allure report to the project

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,18 +1,40 @@
+import allure
+
 from playwright.sync_api import sync_playwright
+
 from pages.login_page import LoginPage
-from utils.config import BASE_URL, TEST_EMAIL,TEST_PASSWORD,SNYK_TOKEN
+
+from utils.config import BASE_URL, TEST_EMAIL, TEST_PASSWORD
 
 
+@allure.feature("Authentication")
+@allure.story("Valid Login")
 def test_login():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
+
         page = browser.new_page()
 
         page.goto(BASE_URL)
 
+        allure.attach(
+            page.screenshot(),
+            name="homepage",
+            attachment_type=allure.attachment_type.PNG
+        )
+
         login_page = LoginPage(page)
+
         login_page.login(TEST_EMAIL, TEST_PASSWORD)
 
-        assert page.locator("text= OWASP Juice Shop").nth(2).is_visible()
+        allure.attach(
+            page.screenshot(),
+            name="after-login",
+            attachment_type=allure.attachment_type.PNG
+        )
+
+        assert page.locator(
+            "button[aria-label='Show/hide account menu']"
+        ).is_visible()
 
         browser.close()
